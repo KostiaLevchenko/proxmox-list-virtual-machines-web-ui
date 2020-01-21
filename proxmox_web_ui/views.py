d1f938142e0070efd2e7cfb9c973e76a5e99f5c1
KostiaLevchenko <sigmund313@gmail.com>
2020-01-21 18:20:55 +0200

    json output

--- proxmox_web_ui/views.py
+++ proxmox_web_ui/views.py
@@ -1,5 +1,6 @@
 from proxmoxer import ProxmoxAPI
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def login(host, password):
@@ -11,6 +12,6 @@ def proxmox(request):
         log_in = login(host=request.POST.get('host'), password=request.POST.get('password'))
         if log_in is not None:
             machines = log_in.nodes.get()
-            return render(request, 'proxymox_web_ui/virtual_machines_list.html', {'machines': machines})
+            return JsonResponse(machines, safe=False)
     return render(request, 'proxymox_web_ui/login.html')
 
