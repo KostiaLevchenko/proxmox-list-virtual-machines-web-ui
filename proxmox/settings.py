cab378dd89de5a5caf982779530db9a4156353cb
KostiaLevchenko <sigmund313@gmail.com>
2020-01-21 17:54:48 +0200

    json output

--- proxmox/settings.py
+++ proxmox/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'proxmox_web_ui'
+    'proxmox_web_ui',
 ]
 
 MIDDLEWARE = [
